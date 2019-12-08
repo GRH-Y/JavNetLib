0c707efd2a9dca28dfbfb3f6a0bf962cb29b8bc4
584335070@qq.com <github-123>
2019-12-08 22:28:49 +0800

    升级jar包，完善解析http响应数据

--- src/main/java/connect/network/xhttp/XHttpReceive.java
+++ src/main/java/connect/network/xhttp/XHttpReceive.java
@@ -2,7 +2,9 @@ package connect.network.xhttp;
 
 import connect.network.nio.NioReceive;
 import connect.network.xhttp.entity.XHttpResponse;
+import storage.GZipUtils;
 import util.DirectBufferCleaner;
+import util.IoEnvoy;
 import util.ReflectionCall;
 import util.StringEnvoy;
 
@@ -29,7 +31,7 @@ public class XHttpReceive extends NioReceive {
         XHttpResponse response = new XHttpResponse();
         try {
             readHttpHead(response, channel);
-            readHttpConntent(response, channel);
+            readHttpContent(response, channel);
         } catch (Exception e) {
             response.setException(e);
             throw e;
@@ -38,56 +40,84 @@ public class XHttpReceive extends NioReceive {
         }
     }
 
-    public void readHttpConntent(XHttpResponse response, SocketChannel channel) throws Exception {
+    public void readHttpContent(XHttpResponse response, SocketChannel channel) throws Exception {
+        String length = response.getHeadForKey(HttpProtocol.XY_CONTENT_LENGTH);
+        String encode = response.getHeadForKey(HttpProtocol.XY_CONTENT_ENCODING);
+        if (StringEnvoy.isNotEmpty(length)) {
+            ByteBuffer buffer = ByteBuffer.allocate(Integer.parseInt(length));
+            try {
+                int ret = IoEnvoy.readToFull(channel, buffer);
+                if (ret != IoEnvoy.FAIL) {
+                    if (StringEnvoy.isNotEmpty(encode) && encode.contains("gzip")) {
+                        //需要解压
+                        response.setRaw(GZipUtils.unCompress(buffer.array()));
+                    } else {
+                        //不需要任何处理，直接保存
+                        response.setRaw(buffer.array());
+                    }
+                }
+            } catch (Exception e) {
+                throw e;
+            } finally {
+                DirectBufferCleaner.clean(buffer);
+            }
+            return;
+        }
+
         ByteBuffer buffer = ByteBuffer.allocate(1);
-        try (ByteArrayOutputStream stream = new ByteArrayOutputStream()) {
+        try (ByteArrayOutputStream stream = new ByteArrayOutputStream();
+             ByteArrayOutputStream slicesStream = new ByteArrayOutputStream()) {
             boolean isExit = false;
+            int isFound = 2;
+            boolean isSlicesData = true;
             do {
                 int ret = channel.read(buffer);
                 if (ret > 0) {
                     stream.write(buffer.array(), 0, buffer.position());
-                    for (int index = 0; index < buffer.position(); index++) {
-                        if (buffer.get(index) == 13) {
-                            if (buffer.get(index + 1) == 10 && buffer.get(index + 2) == 13 && buffer.get(index + 3) == 10) {
-                                //找到http协议分割线
-                                int splitIndex = index + 4;
-                                byte[] data = stream.toByteArray();
-                                String headStr = new String(data, 0, data.length - (buffer.position() - splitIndex));
-                                XHttpResponse.parsingHead(response, headStr);
-                                String length = response.getHeadForKey(HttpProtocol.XY_CONTENT_LENGTH);
-                                if (StringEnvoy.isEmpty(length)) {
-                                    byte[] content = new byte[buffer.position() - splitIndex];
-                                    String[] array = new String(content).split("\r\n");
-                                    System.arraycopy(data, headStr.length(), content, 0, content.length);
-                                    stream.reset();
-                                    stream.write(content);
-                                } else {
+                    if (buffer.get(0) == 13) {
+                        isFound--;
+                    } else if (buffer.get(0) == 10 && isFound == 1) {
+                        if (isSlicesData) {
+                            int slicesDataSize = Integer.parseInt(new String(stream.toByteArray(), 0, stream.size() - 2), 16);
+                            stream.reset();
+                            if (slicesDataSize > 0) {
+                                ByteBuffer slicesBuffer = ByteBuffer.allocate(slicesDataSize);
+                                ret = IoEnvoy.readToFull(channel, slicesBuffer);
+                                if (ret != IoEnvoy.FAIL) {
 
+                                    slicesStream.write(slicesBuffer.array());
+                                    DirectBufferCleaner.clean(slicesBuffer);
                                 }
+                            } else if (slicesDataSize == 0) {
+                                //保存数据
+                                if (slicesStream.size() > 0) {
+                                    if (StringEnvoy.isNotEmpty(encode) && encode.contains("gzip")) {
+                                        //需要解压
+                                        response.setRaw(GZipUtils.unCompress(slicesStream.toByteArray()));
+                                    } else {
+                                        //不需要任何处理，直接保存
+                                        response.setRaw(slicesStream.toByteArray());
+                                    }
+                                }
+                                isSlicesData = false;
                             }
+                            //skip 2 byte \r\n
+                            buffer.clear();
+                            channel.read(buffer);
+                            buffer.clear();
+                            channel.read(buffer);
+                            isFound = 2;
+                        } else {
+                            response.setData(new String(stream.toByteArray()));
+                            isExit = true;
                         }
                     }
-//                    if (buffer.position() > 2 && buffer.get(ret - 2) == 13 && buffer.get(ret - 1) == 10) {
-//                        //找到结束标志
-//                        isFound--;
-//                    } else {
                     buffer.clear();
-//                    }
+                } else {
+                    isExit = true;
                 }
-//                else if (ret == 0) {
-//                    if (stream.size() == 0) {
-//                        isExit = true;
-//                    } else {
-//                        isExit = isFound == 0;
-//                    }
-//                } else {
-//                    if (isFound == 3) {
-//                        throw new IOException("SocketChannel read data exception , It could be a disconnection from the server!!!");
-//                    } else {
-//                        isExit = true;
-//                    }
-//                }
-            } while (channel.isConnected());
+            } while (isExit == false && channel.isConnected());
+
         } catch (Throwable e) {
             if (!(e instanceof SocketTimeoutException)) {
                 throw e;
--- src/main/java/connect/network/xhttp/entity/XHttpResponse.java
+++ src/main/java/connect/network/xhttp/entity/XHttpResponse.java
@@ -76,60 +76,60 @@ public class XHttpResponse {
         }
     }
 
-    public static XHttpResponse parsing(byte[] data) {
-        XHttpResponse response = new XHttpResponse();
-        if (data != null && data.length > 0) {
-            response.setRaw(data);
-            Map<String, String> head = new LinkedHashMap<>();
-            String[] content = new String(data).split("\r\n\r\n");
-            String[] headMap = content[0].split("\r\n");
-            boolean isFirst = true;
-            for (String str : headMap) {
-                if (isFirst) {
-                    isFirst = false;
-                    head.put("response_code: ", str);
-                } else {
-                    String[] args = str.split(": ");
-                    if (args.length == 2) {
-                        head.put(args[0], args[1]);
-                    }
-                }
-            }
-            response.setHeadMap(head);
-            String length = head.get(HttpProtocol.XY_CONTENT_LENGTH);
-            String encode = head.get(HttpProtocol.XY_CONTENT_ENCODING);
-
-            if (StringEnvoy.isEmpty(length)) {
-                int dataStartIndex = content[0].length() + 4;
-                String[] responseData = content[1].split("\r\n");
-                try (ByteArrayOutputStream responseDataStream = new ByteArrayOutputStream()) {
-                    for (int index = 0; index < responseData.length; index += 2) {
-                        if (responseData[index].length() < 8) {
-                            dataStartIndex += responseData[index].length() + 2;
-                            int dataLength = Integer.parseInt(responseData[index], 16);
-                            if (dataLength > 0) {
-                                responseDataStream.write(data, dataStartIndex, dataLength);
-                                dataStartIndex += dataLength + 2;
-                            }
-                        }
-                    }
-                    data = responseDataStream.toByteArray();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
-            if ("gzip".equals(encode)) {
-                byte[] unZip = GZipUtils.unCompress(data);
-                if (unZip != null) {
-                    response.setData(new String(unZip));
-                }
-            } else {
-                response.setData(content[1]);
-            }
-
-        }
-        return response;
-    }
+//    public static XHttpResponse parsing(byte[] data) {
+//        XHttpResponse response = new XHttpResponse();
+//        if (data != null && data.length > 0) {
+//            response.setRaw(data);
+//            Map<String, String> head = new LinkedHashMap<>();
+//            String[] content = new String(data).split("\r\n\r\n");
+//            String[] headMap = content[0].split("\r\n");
+//            boolean isFirst = true;
+//            for (String str : headMap) {
+//                if (isFirst) {
+//                    isFirst = false;
+//                    head.put("response_code: ", str);
+//                } else {
+//                    String[] args = str.split(": ");
+//                    if (args.length == 2) {
+//                        head.put(args[0], args[1]);
+//                    }
+//                }
+//            }
+//            response.setHeadMap(head);
+//            String length = head.get(HttpProtocol.XY_CONTENT_LENGTH);
+//            String encode = head.get(HttpProtocol.XY_CONTENT_ENCODING);
+//
+//            if (StringEnvoy.isEmpty(length)) {
+//                int dataStartIndex = content[0].length() + 4;
+//                String[] responseData = content[1].split("\r\n");
+//                try (ByteArrayOutputStream responseDataStream = new ByteArrayOutputStream()) {
+//                    for (int index = 0; index < responseData.length; index += 2) {
+//                        if (responseData[index].length() < 8) {
+//                            dataStartIndex += responseData[index].length() + 2;
+//                            int dataLength = Integer.parseInt(responseData[index], 16);
+//                            if (dataLength > 0) {
+//                                responseDataStream.write(data, dataStartIndex, dataLength);
+//                                dataStartIndex += dataLength + 2;
+//                            }
+//                        }
+//                    }
+//                    data = responseDataStream.toByteArray();
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//            if ("gzip".equals(encode)) {
+//                byte[] unZip = GZipUtils.unCompress(data);
+//                if (unZip != null) {
+//                    response.setData(new String(unZip));
+//                }
+//            } else {
+//                response.setData(content[1]);
+//            }
+//
+//        }
+//        return response;
+//    }
 
 }
